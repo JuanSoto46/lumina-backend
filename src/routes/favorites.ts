284ee8266eb813591af2b787906c6c473b4302cc
Juan David Soto <juan.soto.garzon@correounivalle.edu.co>
2025-10-23 14:47:33 -0500

    Update favorites.ts extenciones faltantes

--- src/routes/favorites.ts
+++ src/routes/favorites.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { addFavorite, getFavorites, removeFavorite } from "../controllers/favorites.controller.js";
-import { requireAuth } from "../middleware/auth";
+import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
